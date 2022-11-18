84dc0286be697ac1860a0b9107e05f0d76002886
IsmielAbir <ismielabir286@gmail.com>
2022-11-18 19:03:48 +0600

    Update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,30 @@
 import React from 'react';
-
+import './Cart.css'
 const Cart = ({cart, handleRemoveItem}) => {
+
+
+let message;
+if(cart.length === 0){
+    message = <p>Buy At least 1 item :(</p>
+}
+else{
+    message = <p>Thanks For buying :)</p>
+}
+
     return (
         <div>
             <h3>Order Summary</h3>
-            <h5>Order Quantity: {cart.length}</h5>
+            <h5 className={`bold ${cart.length === 2 ? 'blue':'yellow'}`}>Order Quantity: {cart.length}</h5>
             {
                 cart.map(tshirt => <p 
                 key={tshirt._id}
                 >
                     {tshirt.name}
                     <button onClick={() => handleRemoveItem(tshirt)}>X</button>
-                </p>)
+                </p>
+                )
             }
+            {message}
         </div>
     );
 };
